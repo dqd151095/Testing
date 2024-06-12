628f44fdc85e7b5095f8ae93c3581480c85c8fbf
Nguyen Quoc Huy (MS/EDT3-XC) <Huy.NguyenQuoc@vn.bosch.com>
2024-06-12 14:05:45 +0700

    Push bai tap hello world

---
+++ Week_1/Helloworld_NQH.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// #include <stdio.h>
+using namespace std;
+int main()
+{
+
+    cout << "Hello World" << endl;
+    int a=2;
+    cout << "a+2=" <<a+2 << endl;
+    cout<< endl;
+}
