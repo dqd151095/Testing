e34aa38543c374d3aa31121e09cfced9f1e6178e
Vo Anh Tuan (MS/EDT3-XC) <Tuan.VoAnh2@vn.bosch.com>
2024-07-10 22:05:05 +0700

    HelloWorld_VAT

---
+++ Week_1/HelloWorld_VAT.cpp
@@ -0,0 +1,10 @@
+// Same #include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    // Print hello world
+    std::cout << "Hello World" << std::endl;
+    // Print my name
+    std::cout << "I'm Vo Anh Tuan" << std::endl;
+}
